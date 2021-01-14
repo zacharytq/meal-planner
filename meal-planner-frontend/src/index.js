d0e7957227074066c09e3c0293d205508aa76b64
zacharytq <zacharytq@gmail.com>
2021-01-14 14:09:58 -0500

    ul of days, using fetch to populate data

--- meal-planner-frontend/src/index.js
+++ meal-planner-frontend/src/index.js
@@ -1,2 +1,23 @@
 const week = document.getElementById('week');
 
+document.addEventListener('DOMContentLoaded', () => {
+    fetchDays()
+})
+
+function fetchDays() {
+    fetch('http://localhost:3000/days')
+    .then(resp => resp.json())
+    .then(json => makeDays(json))
+}
+
+function makeDays(json) {
+    let list = document.createElement('ul');
+    json.forEach(element => {
+        let li = document.createElement('li')
+        let h2 = document.createElement('h2')
+        h2.innerText = element.name
+        li.appendChild(h2)
+        list.appendChild(li)
+    });
+    week.appendChild(list)
+}
